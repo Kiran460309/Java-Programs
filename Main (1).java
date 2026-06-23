1150681cdd671f6bcba383ec4a209c77965d1661
Kiran.S <kiranshanmugasundaram@gmail.com>
2026-06-23 20:08:30 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=1;
+	    int b=2;
+	    int t=a;
+	    a=b;
+	    b=t;
+	    System.out.println("After swapping: a = " + a + ", b = " + b);
+		
+	}
+}
